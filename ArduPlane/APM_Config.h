d631a7c60197129bc0583825d794d7ab13faf33d
mjj1588 <mjj1588@yahoo.com>
2015-02-20 11:57:13 -0800

    Testing Code Change

--- ArduPlane/APM_Config.h
+++ ArduPlane/APM_Config.h
@@ -4,6 +4,8 @@
 // you wish to change any of the setup parameters from their default
 // values, place the appropriate #define statements here.
 
+// This is a change in the code.
+
 // If you used to define your CONFIG_APM_HARDWARE setting here, it is no
 // longer valid! You should switch to using CONFIG_HAL_BOARD via the HAL_BOARD
 // flag in your local config.mk instead.
